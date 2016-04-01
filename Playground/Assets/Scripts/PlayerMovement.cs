e076fea6777303ca2c8f1bdb6eaa5a7be62e4d73
Germz64 <jeremy64schmidt@gmail.com>
2016-04-02 01:56:56 +0200

    GameUpload
    Unity Project Upload

---
+++ Playground/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody2D rbody;
+    Animator anim;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+	}//start
+
+    // Update is called once per frame
+    void Update() {
+        //Set movementVector to Input Manager's Horizontal and Vertical values (zqsd)
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        if (movementVector != Vector2.zero)
+        {
+            anim.SetBool("iswalking", true);
+            anim.SetFloat("input_x", movementVector.x);
+            anim.SetFloat("input_y", movementVector.y);
+        }
+        else
+        {
+            anim.SetBool("iswalking", false);
+        }//if
+
+        rbody.MovePosition(rbody.position + movementVector * Time.deltaTime * speed);
+	}//Update
+}//PlayerMovement
---
+++ Playground/Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainManager : MonoBehaviour {
+
+    Vector2 itemPOS;
+    ItemDatabase itemDB;
+    // Use this for initialization
+    void Start()
+    {
+        itemDB = new ItemDatabase();
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+    }//Update
+
+    void OnGUI()
+    {
+    //placeItem(new Item("Bronze Sword", 0, "A sword that is made of bronze.", 2, 1, Item.ItemType.Weapon), new Vector2(400, 400));
+    }
+
+    void placeItem(Item item, Vector2 itemPosition)
+    {
+        GUI.DrawTexture(new Rect(itemPosition.x, itemPosition.x, 50, 50), item.itemIcon);
+    }//placeItem
+}
